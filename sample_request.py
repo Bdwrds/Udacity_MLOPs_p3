3f92365b9dda87a142f10b38a38ec324e7cc0848
Bdwrds <edwards.benjamin.james@gmail.com>
2021-12-15 19:16:02 +0000

    make querying more flexible

--- sample_request.py
+++ sample_request.py
@@ -1,44 +1,61 @@
 import requests
 import json
-# pred 0
+import argparse
+import sys
+
 data = {
-    "age": 39,
-    "workclass": "State-gov",
-    "fnlgt": 77516,
-    "education": "Bachelors",
-    "education-num": 13,
-    "marital-status": "Never-married",
-    "occupation": "Adm-clerical",
-    "relationship": "Not-in-family",
-    "race": "White",
-    "sex": "Male",
-    "capital-gain": 2174,
-    "capital-loss": 0,
-    "hours-per-week": 40,
-    "native-country": "United-States"
+    "neg": {
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 2174,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    },
+    "pos": {
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 209642,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 45,
+        "native-country": "United-States"
+    }
 }
-# pred 1
-data2 = {
-    "age": 52,
-    "workclass": "Self-emp-not-inc",
-    "fnlgt": 209642,
-    "education": "HS-grad",
-    "education-num": 9,
-    "marital-status": "Married-civ-spouse",
-    "occupation": "Exec-managerial",
-    "relationship": "Husband",
-    "race": "White",
-    "sex": "Male",
-    "capital-gain": 0,
-    "capital-loss": 0,
-    "hours-per-week": 45,
-    "native-country": "United-States"
-}
 
-#<50k
+def query_api(argue):
+    if argue.api == 'local':
+        address = "http://127.0.0.1:8000/infer/"
+    elif argue.api == 'remote':
+        address = "https://umlops-task.herokuapp.com/infer/"
+    else:
+        print("Must specify local or remote option!")
+        sys.exit()
+    sample_data = data[argue.classified]
+    r = requests.post(address, data=json.dumps(sample_data))
+    print(r.json())
 
 
-address = "http://127.0.0.1:8000/infer/"
-r = requests.post(address, data=json.dumps(data))
-
-print(r.json())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('classified', default='neg',
+                        help='Sample data either pos or neg.')
+    parser.add_argument('api', default='local',
+                        help='Specify local or remote api')
+    args = parser.parse_args()
+    query_api(args)

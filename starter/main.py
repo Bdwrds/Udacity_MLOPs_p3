17e6b0ab1689983ebd6d9db7f04807ba85af1828
Bdwrds <edwards.benjamin.james@gmail.com>
2021-12-15 17:07:33 +0000

    moving req file

--- starter/main.py
+++ starter/main.py
@@ -69,11 +69,3 @@ async def read_data(data: inferData):
     )
     preds = inference(model, XX)
     return json.dumps({"prediction": int(preds)})
-
-@app.post("/items/")
-async def create_item(item: int):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict

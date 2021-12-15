c2f1eaa437211e107ce8bcacb11480a2d487c7c7
Bdwrds <edwards.benjamin.james@gmail.com>
2021-12-15 18:10:11 +0000

    push change

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
+
 sys.path.append(os.path.join(os.getcwd(), 'starter'))
 data = importlib.import_module('ml.data')
 model = importlib.import_module('ml.model')
